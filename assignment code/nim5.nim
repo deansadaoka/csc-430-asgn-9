97e57f3770a150a9348c8c4d4c86086f533c08d2
dsadaoka19 <dsadaoka@calpoly.edu>
2022-03-08 19:35:50 -0800

    Compiles

--- assignment code/nim5.nim
+++ assignment code/nim5.nim
@@ -1,3 +1,6 @@
+import sequtils
+import std/strformat
+
 # Represents ExprC datatype with numC, idC, appC, lamC, ifC, stringC
 type
     ExprTypes = enum
@@ -24,6 +27,8 @@ type
         of stringC: str: string
 
 
+
+# ExprC Test Cases
 let expr1 = ExprC(exp: numC, num: 10.2)
 assert expr1.num == 10.2
 
@@ -35,7 +40,6 @@ assert expr3.fn.sym == "-"
 
 var expr4 = ExprC(exp: lamC, params: @["hello", "world"], body: ExprC(exp: stringC, str: "hey"))
 doAssert expr4.params == @["hello", "world"]
-
 
 let expr5 = ExprC(exp: ifC, cond: false, then: ExprC(exp: numC, num: 5), elseArg: ExprC(exp: numC, num: 10))
 assert expr5.then.num == 5
@@ -61,27 +65,67 @@ type
         of strV: str: string
         of closV:
             params: seq[string]
-            op: string
+            body: ExprC
             env: seq[tuple[name: string, val: Value]]
         of primV: operator: string
+
+
+# Adds args to env
+proc append_env(env: seq[tuple[name: string, val: Value]], params: seq[string], fun_args: seq[Value]): seq[tuple[name: string, val: Value]] = 
+    if params.len == 0:
+        return env
+    elif params.len == 1:
+        var tempBind = @[(params[0], fun_args[0])]
+        return concat(tempBind, env)
+    else:
+        var tempBind = @[(params[0], fun_args[0])]
+        return concat(tempBind, append_env(env, params[1 .. params.len], fun_args[1 .. fun_args.len]))
 
 
 # Interprets the given expression, using the list of funs to resolve applications
 proc interp(exp: ExprC, env: seq[tuple[name: string, val: Value]]) : Value = 
     case exp.exp
     of numC: 
-        echo "it is a num"
+        return Value(valType: numV, num: exp.num)
+    of stringC: 
+        return Value(valType: strV, str: exp.str)
     of idC: 
-        echo "it is an idC"
+        echo "(lookup id env) or lookup(exp.sym, env)"
+    of ifC:
+        case exp.cond
+        of true:
+            return interp(exp.then, env)
+        of false:
+            return interp(exp.elseArg, env)
+        else:
+            echo "if condition not a boolean"
+    of appC:
+        var tempVal = interp(exp.fn, env)
+        case tempVal.valType
+        of closV:
+            var newArgs: seq[Value]
+            for i in 0 .. exp.args.len:
+                newArgs.add(interp(exp.args[i], env))
+            if newArgs.len == tempVal.params.len:
+                return interp(tempVal.body, append_env(tempVal.env, tempVal.params, newArgs))
+            else:
+                echo fmt"TULI5: differing param and arg count {tempVal.params.len} {exp.args.len}"
+        else:
+                echo "put else shit here"
     else:
         echo "invalid type"
     return
 
 
-var exp1 = ExprC(exp: idC, sym: "fat")
+# Interp Test Cases
+var exp1 = ExprC(exp: stringC, str: "fat")
 var env1 = @[("hello", Value(valType: numV, num: 20)), ("world", Value(valType: numV, num: 10))]
+var ret1 = interp(exp1, env1)
+assert ret1.str == "fat"
 
-var ret = interp(exp1, env1)
+var exp2 = ExprC(exp: ifC, cond: false, then: ExprC(exp: stringC, str: "this is true"), elseArg: ExprC(exp: stringC, str: "this is false"))
+var ret2 = interp(exp2, env1)
+assert ret2.str == "this is false"
 
         
 # https://docs.w3cub.com/nim/tut2
@@ -113,3 +157,27 @@ var ret = interp(exp1, env1)
 #                 [(primV op) (primV-interp op a env)]
 #                 [badf (error 'interp "TULI5: not a valid function ~e" badf)])]
 #     [(lamC params body) (closV params body env)]))
+
+
+
+
+
+# (define (lookup [for : Symbol] [env : Environment]) : Value
+#   (match env
+#     ['() (error 'lookup "TULI5: name not found ~e" for)]
+#     [(cons first rest) (if (equal? for (bind-name first)) (bind-val first)
+#            (lookup for rest))]))
+
+
+
+
+# (define extend-env cons)
+
+# ;; Adds args to env
+# (define (append-env [env : Environment] [params : (Listof Symbol)]
+#                     [fun-args : (Listof Value)]) : (Listof bind)
+#   (match params
+#     ['() env]
+#     [_ (extend-env
+#         (bind (first params) (first fun-args))
+#         (append-env env (rest params) (rest fun-args)))]))
